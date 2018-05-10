385e7b8a64f890255440a306f289292b92c5d938
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-10 17:16:23 -0300

    Fix comma missing

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: __dirname + '/public',
         filename: './app.js'
-    }
+    },
     devServer: {
         port: 8181,
         contentBase: './public'
