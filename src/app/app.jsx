1137880b4972c0d430c5f60acf4c50b8efc5f2f9
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-10 17:27:56 -0300

    add menu

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -4,9 +4,12 @@ import 'modules/font-awesome/css/font-awesome.min.css';
 import React from 'react';
 import Todo from '../todo/todo';
 import About from '../about/about';
+import Menu from '../layout/menu';
+
 
 export default props => (
     <div className="container">
+        <Menu />
         <Todo />
         <About />
     </div>
