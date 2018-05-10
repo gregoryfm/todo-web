47396d50a0587860da968d349587fa8e3099a332
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-10 17:38:43 -0300

    Add routes

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,15 +2,13 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
-import Todo from '../todo/todo';
-import About from '../about/about';
+import Routes from './routes';
 import Menu from '../layout/menu';
 
 
 export default props => (
     <div className="container">
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
     </div>
 )
