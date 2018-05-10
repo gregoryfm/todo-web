9aa17dc06075e7b575392bcd306ccfe089de38d7
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-10 17:21:15 -0300

    add todo and about pages

---
+++ src/app/app.jsx
@@ -0,0 +1,13 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+
+import React from 'react';
+import Todo from '../todo/todo';
+import About from '../about/about';
+
+export default props => (
+    <div className="container">
+        <Todo />
+        <About />
+    </div>
+)
---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo</h1>
+            </div>
+        )
+    }
+}
