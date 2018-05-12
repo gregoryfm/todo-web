58eb044446b93e5cdc4433875afdd2e146abdde7
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-12 16:23:13 -0300

    starting form

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 export default props => (
-    <div>
-        <h1>Form</h1>
+    <div role="form" className="todoForm">
+        <div className="col-xs-12 col-sm-9 col-md-10">
+            <input id='description' className='form-control' placeholder='Add a task' />
+        </div>
+
+        <div className='col-xs-12 col-sm-3 col-md-2'>
+            <button className='btn btn-primary'>
+                <i className='fa fa-plus'></i>
+            </button>
+        </div>
     </div>
 )
