722815889866480899845c61534a67b9e2e43afc
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-16 16:15:25 -0300

    using todo-api to save the tasks

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import axios from 'axios';
+
 import PageHeader from '../layout/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
+const URL = 'http://localhost:3003/api/todos';
+
 export default class Todo extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { description: '', list: [] };
+    }
+
     handleAdd() {
-        console.log(this);
+        axios
+            .post(URL, { description: this.state.description })
+            .then(res => console.log("it works"));
+    }
+
+    handleChange( event ) {
+        this.setState({ ...this.state, description: event.target.value });
     }
 
     render() {
         return (
             <div>
                 <PageHeader name="Tasks" small="Register" />
-                <TodoForm handleAdd={() => this.handleAdd()} />
+                <TodoForm 
+                    description={this.state.description}
+                    handleAdd={() => this.handleAdd()}
+                    handleChange={event => this.handleChange(event)} />
                 <TodoList />
             </div>
         )
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -3,13 +3,20 @@ import Grid from '../layout/Grid';
 import IconButton from '../layout/IconButton';
 
 export default props => (
+
+
+
     <div role="form" className="todoForm">
         <Grid cols="12 9 10">
-            <input id='description' className='form-control' placeholder='Add a task' />
+            <input id='description' 
+                className='form-control' 
+                placeholder='Add a task'
+                value={props.description}
+                onChange={props.handleChange} />
         </Grid>
 
         <Grid cols="12 3 2">
-            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd} />
         </Grid>
     </div>
 )
