8525e82e1db43fc413d5a98adea2efb4cd6b07c5
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-24 15:05:49 -0300

    add redux config

---
+++ src/app/reducers.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        list: [{
+            _id: 1,
+            description: 'Pay credit card',
+            done: true,
+        }, {
+            _id: 2,
+            description: 'Meeting with team at 10:00 am',
+            done: false,
+        }, {
+            _id: 3,
+            description: 'Medical appointment on tuesday after lunch',
+            done: false,
+        }]
+    })
+});
+
+export default rootReducer;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './app/app';
+import reducers from './app/reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app')
 )
