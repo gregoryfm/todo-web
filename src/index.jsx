2e9e271259e99a94ecf91778f799b8dcccb51ccb
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-24 17:09:44 -0300

    action creator search and apply redux-promise

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import promise from 'redux-promise'
 
 import App from './app/app';
 import reducers from './app/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducers, devTools);
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
     <Provider store={store}>
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -4,28 +4,38 @@ import { bindActionCreators } from 'redux';
 
 import Grid from '../layout/Grid';
 import IconButton from '../layout/IconButton';
-import { changeDescription } from './todoActions';
+import { changeDescription, search } from './todoActions';
 
-const todoForm = props => (
-    <div role="form" className="todoForm">
-        <Grid cols="12 9 10">
-            <input id='description' 
-                className='form-control' 
-                placeholder='Add a task'
-                value={props.description}
-                onChange={props.changeDescription} />
-        </Grid>
+class TodoForm extends React.Component {
 
-        <Grid cols="12 3 2">
-            <IconButton style='primary' icon='plus' 
-                onClick={props.handleAdd} />
-            <IconButton style='info' icon='search' 
-                onClick={props.handleSearch} />
-        </Grid>
-    </div>
-)
+    componentWillMount() {
+        this.props.search();
+    }
+
+    render() {
+        return (
+            <div role="form" className="todoForm">
+                <Grid cols="12 9 10">
+                    <input id='description' 
+                        className='form-control' 
+                        placeholder='Add a task'
+                        value={this.props.description}
+                        onChange={this.props.changeDescription} />
+                </Grid>
+        
+                <Grid cols="12 3 2">
+                    <IconButton style='primary' icon='plus' 
+                        onClick={this.props.handleAdd} />
+                    <IconButton style='info' icon='search' 
+                        onClick={this.props.handleSearch} />
+                </Grid>
+            </div>
+        )
+    }
+}
 
 const mapStateToProps = state => ({ description: state.todo.description});
-const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch);
+const mapDispatchToProps = dispatch => 
+    bindActionCreators({ changeDescription, search }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(todoForm);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
