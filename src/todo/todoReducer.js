d41573b4e370fa34f210340e56466b34bea6c3ba
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-24 17:11:23 -0300

    remove static value

--- src/todo/todoReducer.js
+++ src/todo/todoReducer.js
@@ -1,19 +1,4 @@
-const INITIAL_STATE = {
-    description: 'Study Redux',
-    list: [{
-        _id: 1,
-        description: 'Pay credit card',
-        done: true,
-    }, {
-        _id: 2,
-        description: 'Meeting with team at 10:00 am',
-        done: false,
-    }, {
-        _id: 3,
-        description: 'Medical appointment on tuesday after lunch',
-        done: false,
-    }]
-}
+const INITIAL_STATE = { description: '', list: [] };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
