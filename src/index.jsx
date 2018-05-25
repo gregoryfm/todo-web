a3771591c10f4d3a4fa27aa7efc2f67368bf49f5
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-25 15:30:26 -0300

    adding redux-multi

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import promise from 'redux-promise'
+import promise from 'redux-promise';
+import multi from 'redux-multi';
 
 import App from './app/app';
 import reducers from './app/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(promise)(createStore)(reducers, devTools);
+const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
     <Provider store={store}>
