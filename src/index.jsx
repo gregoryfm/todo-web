2c5c546484de092ab023029445c9180390348ecf
Gregory Menezes <gregoryfontenele@gmail.com>
2018-05-25 18:07:25 -0300

    adding redux-thunk

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import promise from 'redux-promise';
 import multi from 'redux-multi';
+import thunk from 'redux-thunk';
 
 import App from './app/app';
 import reducers from './app/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools);
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers, devTools);
 
 ReactDOM.render(
     <Provider store={store}>
